c369b0023a53f1385be4f6a47b0edd55c9ade084
murilo <murilra@hotmail.com>
2022-04-11 13:35:57 -0300

    initial commit

---
+++ android/app/src/main/kotlin/com/example/testecarrosel/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.testecarrosel
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/Home.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:carousel_slider/carousel_slider.dart';
+
+class Home extends StatefulWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return CarouselSlider(
+      options: CarouselOptions(height: 400.0),
+      items: [1,2,3,4,5].map((i) {
+        return Builder(
+          builder: (BuildContext context) {
+            return Container(
+                width: MediaQuery.of(context).size.width,
+                margin: const EdgeInsets.symmetric(horizontal: 5.0),
+                decoration: const BoxDecoration(
+                    color: Colors.amber
+                ),
+                child: Text('text $i', style: const TextStyle(fontSize: 16.0),)
+            );
+          },
+        );
+      }).toList(),
+    );
+  }
+}
